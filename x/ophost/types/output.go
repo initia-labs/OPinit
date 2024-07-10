608b512fb4cc998f74c3f42fbdd2d9f68709421f
Hoon <48665813+sh-cha@users.noreply.github.com>
2024-07-10 16:29:34 +0900

    split output calculation (#95)

--- x/ophost/types/output.go
+++ x/ophost/types/output.go
@@ -1,5 +1,12 @@
 package types
 
+import (
+	bytes "bytes"
+	"encoding/binary"
+
+	"golang.org/x/crypto/sha3"
+)
+
 func (output Output) Validate() error {
 	if len(output.OutputRoot) != 32 {
 		return ErrInvalidHashLength.Wrap("output_root")
@@ -10,4 +17,48 @@ func (output Output) Validate() error {
 
 func (output Output) IsEmpty() bool {
 	return len(output.OutputRoot) == 0 && output.L1BlockTime.IsZero() && output.L2BlockNumber == 0
+}
+
+func GenerateOutputRoot(version []byte, stateRoot []byte, storageRoot []byte, latestBlockHash []byte) [32]byte {
+	seed := make([]byte, 32*4)
+	copy(seed, version)
+	copy(seed[32:], stateRoot)
+	copy(seed[64:], storageRoot)
+	copy(seed[96:], latestBlockHash)
+	return sha3.Sum256(seed)
+}
+
+func GenerateWithdrawalHash(bridgeId uint64, l2Sequence uint64, sender string, receiver string, denom string, amount uint64) [32]byte {
+	var withdrawalHash [32]byte
+	seed := []byte{}
+	seed = binary.BigEndian.AppendUint64(seed, bridgeId)
+	seed = binary.BigEndian.AppendUint64(seed, l2Sequence)
+	// variable length
+	seed = append(seed, sender...) // put utf8 encoded address
+	seed = append(seed, Splitter)
+	// variable length
+	seed = append(seed, receiver...) // put utf8 encoded address
+	seed = append(seed, Splitter)
+	// variable length
+	seed = append(seed, denom...)
+	seed = append(seed, Splitter)
+	seed = binary.BigEndian.AppendUint64(seed, amount)
+
+	// double hash the leaf node
+	withdrawalHash = sha3.Sum256(seed)
+	withdrawalHash = sha3.Sum256(withdrawalHash[:])
+
+	return withdrawalHash
+}
+
+func GenerateRootHashFromProofs(data [32]byte, proofs [][]byte) [32]byte {
+	for _, proof := range proofs {
+		switch bytes.Compare(data[:], proof) {
+		case 0, 1: // equal or greater
+			data = sha3.Sum256(append(proof, data[:]...))
+		case -1: // less
+			data = sha3.Sum256(append(data[:], proof...))
+		}
+	}
+	return data
 }
